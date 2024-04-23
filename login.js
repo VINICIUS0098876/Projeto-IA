d6bb34b0c82d388808309cec9dca47e296323887
VINICIUS GUIMARÃES ROBERTO <23111829@CFP127ALUNO.LOCAL>
2024-04-23 17:24:22 -0300

    Terminando

--- login.js
+++ login.js
@@ -2,24 +2,26 @@
 
 
 
-let usuarios = {
-    email: 'vinicius@gmail.com',
-    senha: 'vini1234',
-
-    email: 'caua@gmail.com',
-    senha: 'caua1234',
-
-    email: 'gustavo@gmail.com',
-    senha: 'gustavo1234'
+let fakeBack = {
+    usuarios: [
+        {email: 'vinicius@gmail.com', senha: 'vini1234'},
+        {email: 'caua@gmail.com', senha: 'caua1234'},
+        {email: 'gustavo@gmail.com', senha: 'gustavo1234'}
+    ]
 }
 
 logar.addEventListener('click', function(){
     let emailInput = document.getElementById('email').value;
     let senhaInput = document.getElementById('senha').value;
 
-    if(emailInput === usuarios.email && senhaInput === usuarios.senha){
+    // Verificando se as credenciais fornecidas correspondem a algum usuário no banco de dados falso
+    let usuarioEncontrado = fakeBack.usuarios.find(usuario => usuario.email === emailInput && usuario.senha === senhaInput);
+
+    if(usuarioEncontrado){
+        // Redirecionar para a página home se as credenciais estiverem corretas
         window.location.href = './home.html';
     } else {
-        alert('Preencha os campos corretamente!');
+        // Mostrar mensagem de erro se as credenciais estiverem incorretas
+        alert('Credenciais incorretas. Por favor, tente novamente.');
     }
 })
