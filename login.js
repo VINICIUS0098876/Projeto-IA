052e5fd7ca13797da8fdc43433fbcdebaf3668fc
VINICIUS GUIMARÃES ROBERTO <23111829@CFP127ALUNO.LOCAL>
2024-04-30 17:00:33 -0300

    Terminar

--- login.js
+++ login.js
@@ -1,27 +1,23 @@
-'use strict'
+document.addEventListener("DOMContentLoaded", async function() {
+    const logarBtn = document.getElementById("logar");
+    logarBtn.addEventListener("click", async function() {
+        const email = document.getElementById("email").value;
+        const senha = document.getElementById("senha").value;
 
+        try {
+            const response = await fetch("https://back-login.vercel.app/usuarios");
+            const usuarios = await response.json();
 
+            const usuario = usuarios.find(user => user.email === email && user.senha === senha);
 
-let fakeBack = {
-    usuarios: [
-        {email: 'vinicius@gmail.com', senha: 'vini1234'},
-        {email: 'caua@gmail.com', senha: 'caua1234'},
-        {email: 'gustavo@gmail.com', senha: 'gustavo1234'}
-    ]
-}
-
-logar.addEventListener('click', function(){
-    let emailInput = document.getElementById('email').value;
-    let senhaInput = document.getElementById('senha').value;
-
-    // Verificando se as credenciais fornecidas correspondem a algum usuário no banco de dados falso
-    let usuarioEncontrado = fakeBack.usuarios.find(usuario => usuario.email === emailInput && usuario.senha === senhaInput);
-
-    if(usuarioEncontrado){
-        // Redirecionar para a página home se as credenciais estiverem corretas
-        window.location.href = './home.html';
-    } else {
-        // Mostrar mensagem de erro se as credenciais estiverem incorretas
-        alert('Credenciais incorretas. Por favor, tente novamente.');
-    }
-})
+            if (usuario) {
+                alert("Login feito com sucesso!!");
+                window.location.href = './home.html'
+            } else {
+                alert("Escreva as credenciais corretas!!");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    });
+});
